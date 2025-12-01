f83c8ff9c42353d1fd4bb472fd02ea4601d01a9a
muddglobb <dhimassulistio2@gmail.com>
2025-12-01 09:51:00 +0700

    fix: config issue, change from js to ts

--- week4_integration/web/src/app/page.js
+++
@@ -1,8 +0,0 @@
-
-export default function Home() {
-  return (
-    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans ">
-      <p> HI</p>
-    </div>
-  );
-}
---
+++ week4_integration/web/src/app/page.tsx
@@ -0,0 +1,12 @@
+import Navbar from "@/components/navbar";
+
+export default function Home() {
+  return (
+    <>
+      <Navbar />
+      <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans ">
+        <p> HI</p>
+      </div>
+    </>
+  );
+}
