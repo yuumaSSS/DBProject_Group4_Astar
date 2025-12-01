fea5618167d0af7a4f9804aeb50d51eefc65ee47
muddglobb <dhimassulistio2@gmail.com>
2025-12-01 11:44:05 +0700

    feat: add nav bar

--- week4_integration/web/src/components/navbar.tsx
+++ week4_integration/web/src/components/navbar.tsx
@@ -1,9 +1,22 @@
-import Container from './container'
+import Container from "./container";
+import Image from "next/image";
+import { Search, ShoppingCart, User } from "lucide-react";
 
 const Navbar = () => {
   return (
-    <Container className='bg-red-400'>Navbar</Container>
-  )
-}
+    <div className="shadow-sm">
+      <Container className="flex flex-row h-20 py-1 items-center justify-between">
+        <div className="h-full aspect-square relative">
+          <Image src="/AStar-Logo.webp" alt="Logo" width={72} height={72} />
+        </div>
+        <div className="flex gap-8 text-black">
+          <Search />
+          <ShoppingCart />
+          <User />
+        </div>
+      </Container>
+    </div>
+  );
+};
 
-export default Navbar
+export default Navbar;
